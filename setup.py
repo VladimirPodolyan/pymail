1fba8bf53d75aab886310e2bdf3d6b21b9fb1355
PVladimir <vladimir.podolyan@techops.ru>
2019-06-16 12:20:53 +0300

    del extras

--- setup.py
+++ setup.py
@@ -14,9 +14,6 @@ setuptools.setup(
     url="https://github.com/VladimirPodolyan/pymail",
     packages=setuptools.find_packages(),
     install_requires=['pytest>=4.4'],
-    extras_require={
-        'test': 'pip install -e "git+git@github.com:VladimirPodolyan/pymail.git@add-setup#egg=pymail-0.0.1"'
-    },
     classifiers=[
         "Programming Language :: Python :: 3"
     ],
